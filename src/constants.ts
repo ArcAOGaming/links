1767458365778cbe47839954330bd3dc970e2097
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-05-09 19:26:21 -0400

    better links for seo

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,9 @@
 // ARNS Gateway
-export const DEFAULT_ARNS_GATEWAY = 'arweave.net';
+const getGateWay = () => {
+    return ARNS_GATEWAYS[Math.floor(Math.random() * ARNS_GATEWAYS.length)];
+}
+
+export const DEFAULT_ARNS_GATEWAY = getGateWay();
 
 //ARNS Names
 export const ARNS_RANDAO = 'randao'
@@ -24,3 +28,31 @@ export const DOMAIN_YOUTUBE_HANDLE = 'youtube.com/@';
 // Shared Social Links
 export const ARCAO_DISCORD = 'arc-ao';
 export const ARCAO_TELEGRAM = 'ArcAOGames';
+
+
+const ARNS_GATEWAYS = [
+    "enzifiri.com",
+    "ario-gateway.nethermind.dev",
+    "ar01.fyeo.io",
+    "arweave.zelf.world",
+    "cyanalp.cfd",
+    "openar.dev",
+    "siyantest.xyz",
+    "zaurmammadov.xyz",
+    "permadao.io",
+    "rtmpsunucu.online",
+    "flechemano.com",
+    "arweave.ar",
+    "bambik.online",
+    "devkral.xyz",
+    "adn79.pro",
+    "permagate.io",
+    "asilkan.online",
+    "g8way.io",
+    "zigza.xyz",
+    "canduesed.me",
+    "krayir.xyz",
+    "0xyvz.xyz",
+    "anaraydinli.xyz",
+    "defi.ao"
+]
