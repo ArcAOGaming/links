e3a54266217977f80988c3a8a8305cef02edd24f
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-05-09 19:32:10 -0400

    fix gateway

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,6 @@
-// ARNS Gateway
-const getGateWay = () => {
-    return ARNS_GATEWAYS[Math.floor(Math.random() * ARNS_GATEWAYS.length)];
-}
+import { getGateWay } from "./gateways";
 
+// ARNS Gateway
 export const DEFAULT_ARNS_GATEWAY = getGateWay();
 
 //ARNS Names
@@ -30,29 +28,3 @@ export const ARCAO_DISCORD = 'arc-ao';
 export const ARCAO_TELEGRAM = 'ArcAOGames';
 
 
-const ARNS_GATEWAYS = [
-    "enzifiri.com",
-    "ario-gateway.nethermind.dev",
-    "ar01.fyeo.io",
-    "arweave.zelf.world",
-    "cyanalp.cfd",
-    "openar.dev",
-    "siyantest.xyz",
-    "zaurmammadov.xyz",
-    "permadao.io",
-    "rtmpsunucu.online",
-    "flechemano.com",
-    "arweave.ar",
-    "bambik.online",
-    "devkral.xyz",
-    "adn79.pro",
-    "permagate.io",
-    "asilkan.online",
-    "g8way.io",
-    "zigza.xyz",
-    "canduesed.me",
-    "krayir.xyz",
-    "0xyvz.xyz",
-    "anaraydinli.xyz",
-    "defi.ao"
-]
---
+++ src/gateways.ts
@@ -0,0 +1,32 @@
+
+export const getGateWay = () => {
+    return ARNS_GATEWAYS[Math.floor(Math.random() * ARNS_GATEWAYS.length)];
+}
+
+
+const ARNS_GATEWAYS = [
+    "enzifiri.com",
+    "ario-gateway.nethermind.dev",
+    "ar01.fyeo.io",
+    "arweave.zelf.world",
+    "cyanalp.cfd",
+    "openar.dev",
+    "siyantest.xyz",
+    "zaurmammadov.xyz",
+    "permadao.io",
+    "rtmpsunucu.online",
+    "flechemano.com",
+    "arweave.ar",
+    "bambik.online",
+    "devkral.xyz",
+    "adn79.pro",
+    "permagate.io",
+    "asilkan.online",
+    "g8way.io",
+    "zigza.xyz",
+    "canduesed.me",
+    "krayir.xyz",
+    "0xyvz.xyz",
+    "anaraydinli.xyz",
+    "defi.ao"
+]
